a51a5a948dad82639dd02e9bbdc552dda8fe32c1
unnati-078 <unnaticodes07@gmail.com>
2025-08-18 20:40:40 +0530

    changes in memoryaddress.cpp

--- day09/memoryaddress.cpp
+++ day09/memoryaddress.cpp
@@ -5,10 +5,12 @@ int main() {
     int a = 5;
     cout << &a << endl;
     int *ptr = &a;
-    cout << ptr<<endl;
+    cout << ptr << endl;
 
-    //pointer to pointer
-    cout<<"\n"
-    int **ptr2= &ptr;
-    cout << ptr2
+    // pointer to pointer
+    cout << "\n";
+    int **ptr2 = &ptr;
+    cout << ptr2 << endl;
+
+    return 0;
 }
