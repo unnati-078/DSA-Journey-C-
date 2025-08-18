99e6dc542b15d9056a7411b61c97d5ea146548be
unnati-078 <unnaticodes07@gmail.com>
2025-08-18 20:35:59 +0530

    changes in day9

---
+++ day09/memoryaddress.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    cout << &a << endl;
+    int *ptr = &a;
+    cout << ptr<<endl;
+
+    //pointer to pointer
+    cout<<"\n"
+    int **ptr2= &ptr;
+    cout << ptr2
+}
